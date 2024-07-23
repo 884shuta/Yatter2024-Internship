158af41c6cd0234d7126b0a438cbd2b4a55e7328
@v2258250 <v2258250@oita-u.ac.jp>
2024-07-23 16:50:50 +0900

    Merge pull request #8 from mitohato14/fix/template
    テンプレートプロジェクトをテンプレート用に修正

--- Yatter2024/app/src/main/java/com/dmm/bootcamp/yatter2024/ui/timeline/PublicTimelineViewModel.kt
+++ Yatter2024/app/src/main/java/com/dmm/bootcamp/yatter2024/ui/timeline/PublicTimelineViewModel.kt
@@ -1,12 +1,15 @@
 package com.dmm.bootcamp.yatter2024.ui.timeline
 
 import androidx.lifecycle.ViewModel
+import com.dmm.bootcamp.yatter2024.domain.repository.StatusRepository
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 
-class PublicTimelineViewModel : ViewModel() {
-    private val _uiState: MutableStateFlow<PublicTimelineUiState> =
-        MutableStateFlow(PublicTimelineUiState.empty())
+
+class PublicTimelineViewModel(
+    private val statusRepository: StatusRepository,
+) : ViewModel() {
+    private val _uiState: MutableStateFlow<PublicTimelineUiState> = MutableStateFlow(PublicTimelineUiState.empty())
     val uiState: StateFlow<PublicTimelineUiState> = _uiState.asStateFlow()
 }
